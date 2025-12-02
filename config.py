12fcb21d489b2c4da96c024c46d18ac8163a58ad
Samuelhizkya <semyulhizkya@gmail.com>
2025-12-02 16:02:00 +0800

    Add configuration file for Twitter API settings

---
+++ config.py
@@ -0,0 +1,9 @@
+# ===============================
+# FILE: config.py
+# ===============================
+
+BEARER_TOKEN = "AAAAAAAAAAAAAAAAAAAAAMK35gEAAAAA8NDtKysthb8uEciGcm8y4UKQ6uA%3D3HCMUopWE0uupZPpZlwJTSdcWOKwSyXf8qEg1Xqqhlvn8I9esO"
+
+QUERY = "gempa OR banjir OR longsor OR kebakaran lang:id"
+
+MAX_TWEETS = 10
